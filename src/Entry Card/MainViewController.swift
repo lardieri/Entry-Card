5becf64a36b789d1994f017ceef4e362f76e93f9
lardieri <lardieri@users.noreply.github.com>
2021-06-18 22:35:10 -0700

    Create Xcode project.

---
+++ src/Entry Card/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  Entry Card
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
