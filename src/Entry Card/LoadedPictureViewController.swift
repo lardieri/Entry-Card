0b9f623a35ee2de8bd12d11f1270e2ec1f26ffa4
lardieri <lardieri@users.noreply.github.com>
2021-06-20 03:48:51 -0700

    Set min/max zoom factors based on image size.

--- src/Entry Card/LoadedPictureViewController.swift
+++ src/Entry Card/LoadedPictureViewController.swift
@@ -14,28 +14,54 @@ class LoadedPictureViewController: UIViewController {
         super.viewDidLoad()
 
         scrollView!.delegate = self
+
         imageView?.image = loadedPicture?.image
+        adjustZoomFactors()
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+
+        adjustZoomFactors()
         
         if let scrollView = scrollView {
             centerScrollViewIfNeeded(scrollView)
         }
     }
 
-    private func centerScrollViewIfNeeded(_ scrollView: UIScrollView) {
-        guard scrollView.contentSize.notEmpty else { return }
+    private func adjustZoomFactors() {
+        guard let scrollView = self.scrollView else { return }
+        guard let _ = self.imageView else { return }
+        guard let image = loadedPicture?.image else { return }
 
-        let insetX = max(0.0, (scrollView.bounds.width - scrollView.contentSize.width) / 2.0)
-        let insetY = max(0.0, (scrollView.bounds.height - scrollView.contentSize.height) / 2.0)
-        scrollView.contentInset = UIEdgeInsets(top: insetY, left: insetX, bottom: 0.0, right: 0.0)
+        let imageSize = image.size
+        let viewSize = scrollView.frame.size
+
+        guard imageSize.notEmpty && viewSize.notEmpty else { return }
+
+        let minZoom = 0.95 * min(viewSize.height / imageSize.height, viewSize.width / imageSize.width)
+        let maxZoom = 4.0 * minZoom
+
+        scrollView.minimumZoomScale = minZoom
+        scrollView.maximumZoomScale = maxZoom
+
+        scrollView.setZoomScale(minZoom, animated: true)
+    }
+
+    private func centerScrollViewIfNeeded(_ scrollView: UIScrollView) {
+        OperationQueue.main.addOperation {
+            guard scrollView.contentSize.notEmpty else { return }
+
+            let insetX = max(0.0, (scrollView.bounds.width - scrollView.contentSize.width) / 2.0)
+            let insetY = max(0.0, (scrollView.bounds.height - scrollView.contentSize.height) / 2.0)
+            scrollView.contentInset = UIEdgeInsets(top: insetY, left: insetX, bottom: 0.0, right: 0.0)
+        }
     }
 
     var loadedPicture: StorageManager.LoadedPicture? {
         didSet {
             imageView?.image = loadedPicture?.image
+            adjustZoomFactors()
         }
     }
 
