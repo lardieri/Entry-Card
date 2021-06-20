6f94cc1ecb0ecd8b910903f4ce3deaae35977a5f
lardieri <lardieri@users.noreply.github.com>
2021-06-20 02:00:26 -0700

    Center the loaded picture when it's smaller than the scroll view.

--- src/Entry Card/LoadedPictureViewController.swift
+++ src/Entry Card/LoadedPictureViewController.swift
@@ -17,6 +17,22 @@ class LoadedPictureViewController: UIViewController {
         imageView?.image = loadedPicture?.image
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        if let scrollView = scrollView {
+            centerScrollViewIfNeeded(scrollView)
+        }
+    }
+
+    private func centerScrollViewIfNeeded(_ scrollView: UIScrollView) {
+        guard scrollView.contentSize.notEmpty else { return }
+
+        let insetX = max(0.0, (scrollView.bounds.width - scrollView.contentSize.width) / 2.0)
+        let insetY = max(0.0, (scrollView.bounds.height - scrollView.contentSize.height) / 2.0)
+        scrollView.contentInset = UIEdgeInsets(top: insetY, left: insetX, bottom: 0.0, right: 0.0)
+    }
+
     var loadedPicture: StorageManager.LoadedPicture? {
         didSet {
             imageView?.image = loadedPicture?.image
@@ -30,6 +46,19 @@ extension LoadedPictureViewController: UIScrollViewDelegate {
 
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imageView
+    }
+
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        centerScrollViewIfNeeded(scrollView)
+    }
+
+}
+
+
+extension CGSize {
+
+    var notEmpty: Bool {
+        return self.width > 0.0 && self.height > 0.0
     }
 
 }
