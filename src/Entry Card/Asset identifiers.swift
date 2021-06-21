5863cd2c826301172cdde03aae90e4907c09ff78
lardieri <lardieri@users.noreply.github.com>
2021-06-21 03:06:09 -0700

    Add the Settings button.

---
+++ src/Entry Card/Asset identifiers.swift
@@ -0,0 +1,19 @@
+//
+//  Asset identifiers.swift
+//  Entry Card
+//
+
+import Foundation
+
+enum Images {
+
+    static let gearshape = "gearshape"
+
+}
+
+
+enum Colors {
+
+    static let settingsButton = "Settings Button Color"
+
+}
--- src/Entry Card/Storyboard identifiers.swift
+++ src/Entry Card/Storyboard identifiers.swift
@@ -9,6 +9,7 @@ enum Storyboard {
 
     static let pageViewControllerEmbeddedSegue = "Page View Controller embedded segue"
     static let emptyPictureViewController = "Empty Picture View Controller"
+    static let presentSettingsSegue = "Present Settings segue"
     static let settingsDismissedSegue = "Settings Dismissed Segue"
 
 }
